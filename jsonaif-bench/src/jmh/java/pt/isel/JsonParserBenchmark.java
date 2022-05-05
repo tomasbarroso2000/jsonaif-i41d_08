9fe5638223c1fae839c4a98e429f5e16fe391022
tomasbarroso2000 <tomasbarroso2000@gmail.com>
2022-05-05 11:32:18 +0100

    start of benchmark

--- jsonaif-bench/src/jmh/java/pt/isel/JsonParserBenchmark.java
+++ jsonaif-bench/src/jmh/java/pt/isel/JsonParserBenchmark.java
@@ -1,5 +1,7 @@
 package pt.isel;
 
+import kotlin.reflect.KClass;
+import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
@@ -9,5 +11,20 @@ import java.util.concurrent.TimeUnit;
 @BenchmarkMode(Mode.Throughput)
 @OutputTimeUnit(TimeUnit.MILLISECONDS)
 public class JsonParserBenchmark {
+    static final String json = "{ name: \"Ze Manel\", sibling: {name: \"Ze Shrek\"} }";
+    static final JsonParserReflect jsonParserReflect = JsonParserReflect.INSTANCE;
 
+    static final JsonParserDynamic jsonParserDynamic = JsonParserDynamic.INSTANCE;
+
+    static final KClass<Person> personKlass = kotlin.jvm.JvmClassMappingKt.getKotlinClass(Person.class);
+
+    @Benchmark
+    public void benchJsonParserStudentViaReflection() {
+        jsonParserReflect.parse(json, personKlass);
+    }
+
+    @Benchmark
+    public void benchJsonParserStudentViaDynamic() {
+        jsonParserDynamic.parse(json, personKlass);
+    }
 }
